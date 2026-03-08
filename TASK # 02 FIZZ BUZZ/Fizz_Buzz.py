c317834bb9e90bf96944af1ffac0af09574d711f
Jahanzaib Aziz <su92-bsaim-s25-047@superior.edu.pk>
2026-03-08 13:14:23 +0500

    Add files via upload

---
+++ TASK # 02 FIZZ BUZZ/Fizz_Buzz.py
@@ -0,0 +1,20 @@
+# Fizz Buzz Program
+class FizzBuzz:
+    def __init__(self, limit):
+        self.limit = limit
+
+    def run(self):
+        for i in range(1, self.limit + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                print("FizzBuzz")
+            elif i % 3 == 0:
+                print("Fizz")
+            elif i % 5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+
+
+# Example
+obj = FizzBuzz(100)
+obj.run()
